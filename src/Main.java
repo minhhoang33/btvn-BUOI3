5af864be41d8964aab7f22223f1c64929573f3de
minhhoang33 <hoanggok@gmail.com>
2024-10-02 19:29:15 +0700

    bai-tap-3

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.awt.font.NumericShaper;
+import java.lang.reflect.Array;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        double t1 = 2.25;
+        int v1 = 54;
+        int v2 = 38;
+        double s = ((t1*v1)+(t1*v2));
+        System.out.println(s);
+
+
+            double v3 = 4.2 ;
+            double t3 = 2.5;
+            double v4 = 4.2*(5/2);
+            double s1 =(v3*t3);
+            double t2 = (s1/v4);
+            System.out.println(t2);
+
+
+    }
+}
---
+++ src/NewClass.java
@@ -0,0 +1,6 @@
+public class NewClass {
+    /*quy tắc đặt tên class:
+    * Viết hoa chữ cái đầu
+    *Viết theo quy tắc con lạc đà và viết liền
+    * tên class phải là danh từ*/
+}
